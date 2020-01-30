c9eb2912bfd0b0bb07ef43636ccca98eda016ee6
Tuan <tuanhaviet22@gmail.com>
2020-01-30 18:33:40 +0700

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,50 +1,75 @@
-
 export default {
   name: 'home',
   components: {
 
   },
   props: [
-    
+
   ],
-  data () {
+  data() {
     return {
-      info : null 
+      info: null
+    }
+  },
+  watch : {
+    info(){
+      this.loadDetail()
     }
   },
   computed: {
+   
+  },
+ 
+  created() {
+    this.getList()
+  },
+  mounted() {
+    //  this.$axios
+    // .get(this.$api.pokeapi + 'pokemon?limit=10')
+    // .then(response => (this.info = response.data.results))
+    // .then( ()=> {
+    //   this.info.forEach(item => {
+    //     this.$axios
+    //     .get(item.url)
+    //     .then( function(res) {
+    //       item.detail = res.data
+    //       item.image = 'https://pokeres.bastionbot.org/images/pokemon/'+ res.data.id + ".png"
+    //     })
+    //   })
+    // })
+  },
+  beforeUpdate() {
 
   },
-  beforeCreate() {
-    
-  },
-  created(){
-    
-  },
-  mounted () {
-     this.$axios
-    .get(this.$api.pokeapi + 'pokemon?limit=10')
-    .then(response => (this.info = response.data.results))
-    .then( ()=> {
-      this.info.forEach(item => {
-        this.$axios
-        .get(item.url)
-        .then( function(res) {
-          item.detail = res.data
-          item.image = 'https://pokeres.bastionbot.org/images/pokemon/'+ res.data.id + ".png"
-        })
-      })
-    })
-  },
-  beforeUpdate(){
-    
-  },
-  updated(){
-    
+  updated() {
+
   },
   methods: {
-
+    getList: function () {
+      const vm = this
+      this.$axios
+        .get(this.$api.pokeapi + 'pokemon?limit=10')
+        .then(response => (vm.info = response.data.results));
+    },
+    loadDetail: function () {
+      this.info.forEach(item => {
+        var detailUrl = item.url
+        this.getDetail(detailUrl)
+      })
+    },
+    getDetail: function (url) {
+      this.$axios.get(url).then(res => {     
+        this.mapDetailToInfo(res.data)
+      })
+    },
+    mapDetailToInfo: function (res) {
+      this.info = this.info.map(item => {
+        item.detail = res
+      })
+      this.info = res
+    }
+    // handleData : function(){
+    //   this.$set(this.info)
+    // }
   }
 }
-
-
